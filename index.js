f37bc2bb3726ff0a4ed40e37e34942e4b4b23f72
nlsnlaurensius <nelson.laurensius@ui.ac.id>
2025-05-04 23:19:59 +0700

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const { errorResponse } = require('./src/utils/baseResponse.util');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -25,10 +31,7 @@ app.use((err, req, res, next) => {
     res.status(500).json(errorResponse('Something went wrong!', err.message));
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
 
 db.connect()
   .then(() => console.log('Database connected successfully'))
